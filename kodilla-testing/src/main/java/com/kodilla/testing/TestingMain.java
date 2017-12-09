e8d246e91fa2d6b06cb81caee438c9fae15274fb
woitech <wojlow@gmail.com>
2017-12-09 09:37:46 +0100

    Zadanie: Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,47 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.print("Testing of SimpleUser(String): ");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            passed();
+        } else {
+            failed();
+        }
+
+        // Calculator tests
+        Calculator calc = new Calculator();
+
+        System.out.print("Testing of Calculator.add(int,int): ");
+        final int e1 = Integer.MAX_VALUE-1;
+        final int e2 = 1;
+        final int sum = Integer.MAX_VALUE;
+        if (calc.add(e1, e2) == sum) {
+            passed();
+        } else {
+            failed();
+        }
+
+        System.out.print("Testing of Calculator.substract(int,int): ");
+        final int minuend = Integer.MIN_VALUE+1;
+        final int subtrahend = 1;
+        final int diff = Integer.MIN_VALUE;
+        if (calc.subtract(minuend, subtrahend) == diff) {
+            passed();
+        } else {
+            failed();
+        }
     }
+
+    private static void passed() {
+        System.out.println("Test Ok");
+    }
+
+    private static void failed() { System.out.println("Error!"); }
+
 }
