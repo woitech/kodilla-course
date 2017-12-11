35ae97f6a28d61bdab128e243bf876b10b2a4321
woitech <wojlow@gmail.com>
2017-12-11 19:09:03 +0100

    CollectionTestSuite changed

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -6,8 +6,6 @@ import java.util.*;
 import static org.junit.Assert.*;
 
 public class CollectionTestSuite {
-    private OddNumbersExterminator exterm;
-
     @BeforeClass
     public static void beforeClass() {
         report("CollectionTestSuite BEGIN");
@@ -21,7 +19,6 @@ public class CollectionTestSuite {
     @Before
     public void before() {
         report("Test unit BEGIN");
-        exterm = new OddNumbersExterminator();
     }
 
     @After
@@ -31,21 +28,50 @@ public class CollectionTestSuite {
 
     @Test
     public void testOddNumbersExterminatorEmptyList() {
-        ArrayList<Integer> result = exterm.exterminate(new ArrayList<>());
+        // GIVEN
+        OddNumbersExterminator exterm = new OddNumbersExterminator();
+        ArrayList<Integer> emptyList = new ArrayList<>();
+
+        //WHEN
+        ArrayList<Integer> result = exterm.exterminate(emptyList);
+
+        //THEN
         assertNotNull(result);
         assertTrue(result.isEmpty());
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList() {
-        ArrayList<Integer> expected =
-            new ArrayList<>(Arrays.asList(100, 2, -2, 0, 4, -4, 100));
-        ArrayList<Integer> inputData =
-            new ArrayList<>(Arrays.asList(-1, 100, 3, 2, -5, -2, 7,
-                                           0, -9, 4, 11, -4, -13, 100));
-        ArrayList<Integer> result = exterm.exterminate(inputData);
+        //GIVEN
+        OddNumbersExterminator exterm = new OddNumbersExterminator();
+        ArrayList<Integer> averageList =
+                new ArrayList<>(Arrays.asList(-1, 100, 3, 2, -5, -2, 7,
+                        0, -9, 4, 11, -4, -13, 100));
+
+        //WHEN
+        ArrayList<Integer> result = exterm.exterminate(averageList);
+
+        //THEN
         assertNotNull(result);
+        ArrayList<Integer> expected =
+                new ArrayList<>(Arrays.asList(100, 2, -2, 0, 4, -4, 100));
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void testOddNumbersExterminatorIllegalArgument() {
+        //GIVEN
+        OddNumbersExterminator exterm = new OddNumbersExterminator();
+        ArrayList<Integer> invalidList = null;
+
+        try {
+            //WHEN
+            exterm.exterminate(invalidList);
+            //THEN
+            fail();
+        } catch (IllegalArgumentException exc) {
+            //OK
+        }
     }
 
     private static void report(String msg) {
