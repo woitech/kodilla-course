3ba2c6a20ecdbb92d8563cd5e0b74b9fcd97a76a
woitech <wojlow@gmail.com>
2017-12-23 09:19:17 +0100

    Exception handling

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,24 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+        Path path = Paths.get(file.getPath());
+        //Path path = Paths.get("s");
+        try (Stream<String> fileLines = Files.lines(path)) {
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Oh, no! Something went wrong! Error:" + e);
+        } finally {
+            System.out.println("I'm gonna be here... always!");
+        }
+    }
+}
