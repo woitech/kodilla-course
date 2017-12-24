87a3fb1f85c583c08dd1bb3c0af7772ff730fb4f
woitech <wojlow@gmail.com>
2017-12-24 01:33:09 +0100

    Zadanie: Wiedza o wyjątkach

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Flights.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Flights {
+    public static void main(String[] args) {
+        String departureAirport = "Warsaw Airport";
+        Set<Flight> flights = new HashSet<>();
+        flights.add(new Flight(departureAirport, "Copenhagen Airport"));
+        flights.add(new Flight(departureAirport, "London City Airport"));
+        flights.add(new Flight(departureAirport, "Bangkok Suvarnabhumi"));
+        flights.add(new Flight(departureAirport, "Oslo Airport"));
+        flights.add(new Flight(departureAirport, "Disneyland Airport"));
+        flights.add(new Flight(departureAirport, "Lisbon Airport"));
+        flights.add(new Flight(departureAirport, "Dubai Airport"));
+
+        FlightSchedule schedule = new FlightSchedule();
+        for (Flight flight : flights) {
+            try {
+                System.out.printf("%s: %s\n", flight,
+                    schedule.findFilght(flight) ? "SCHEDULED" : "CANCELED");
+            } catch (RouteNotFoundException e) {
+                System.out.printf("[Error: %s does not exist]\n", flight);
+            }
+        }
+    }
+}
