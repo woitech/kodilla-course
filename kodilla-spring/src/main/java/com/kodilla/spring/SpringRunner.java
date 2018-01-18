8ff53026873ae975a05a63f2c7cd6b1ebd6b2b55
woitech <wojlow@gmail.com>
2018-01-18 23:05:13 +0100

    Konfiguracja przy pomocy klasy Java

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -1,9 +1,13 @@
 package com.kodilla.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan
+//@EnableAutoConfiguration
 public class SpringRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRunner.class, args);
