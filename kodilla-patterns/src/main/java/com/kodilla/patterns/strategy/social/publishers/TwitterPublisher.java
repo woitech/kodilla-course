543f49a124a561c0e22f96352fb9dffb8cdaa087
woitech <wojlow@gmail.com>
2018-01-21 16:34:54 +0100

    Zadanie: Media społecznościowe użytkowników

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/publishers/TwitterPublisher.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns.strategy.social.publishers;
+
+import com.kodilla.patterns.strategy.social.SocialPublisher;
+
+import static com.kodilla.patterns.Validator.validateString;
+
+public class TwitterPublisher implements SocialPublisher {
+    @Override
+    public String share(String userName, String content) {
+        validateString(userName, "valueless userName");
+        validateString(content, "valueless content");
+
+        return String.format("[TWITTER] %s writes: '%s'", userName, content);
+    }
+}
