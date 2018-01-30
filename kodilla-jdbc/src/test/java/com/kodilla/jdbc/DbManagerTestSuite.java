da93e7cdcf2ea257b88fb0de9fac7ee3466bea2b
woitech <wojlow@gmail.com>
2018-01-30 19:57:19 +0100

    SQL query updated

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -49,11 +49,11 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
 
         //When
-        String sqlQuery = "SELECT U.FIRSTNAME AS FIRST_NAME, U.LASTNAME AS LAST_NAME, COUNT(U.ID) AS POSTS " +
+        String sqlQuery = "SELECT U.FIRSTNAME AS FIRST_NAME, U.LASTNAME AS LAST_NAME, COUNT(U.ID) AS USER_POSTS " +
                           "FROM USERS U JOIN POSTS P ON U.ID = P.USER_ID " +
-                          "GROUP BY U.ID, U.FIRSTNAME, U.LASTNAME " +
-                          "HAVING POSTS >= 2 " +
-                          "ORDER BY LAST_NAME, FIRST_NAME, POSTS";
+                          "GROUP BY U.ID " +
+                          "HAVING USER_POSTS >= 2 " +
+                          "ORDER BY LAST_NAME, FIRST_NAME, USER_POSTS";
         Statement statement = dbManager.getConnection().createStatement();
         ResultSet rs = statement.executeQuery(sqlQuery);
         ResultSetMetaData rsmd = rs.getMetaData();
@@ -62,10 +62,10 @@ public class DbManagerTestSuite {
         assertEquals(3, rsmd.getColumnCount());
         assertEquals("FIRSTNAME", rsmd.getColumnName(1));
         assertEquals("LASTNAME", rsmd.getColumnName(2));
-        assertEquals("POSTS", rsmd.getColumnName(3));
+        assertEquals("USER_POSTS", rsmd.getColumnName(3));
         assertEquals("FIRST_NAME", rsmd.getColumnLabel(1));
         assertEquals("LAST_NAME", rsmd.getColumnLabel(2));
-        assertEquals("POSTS", rsmd.getColumnLabel(3));
+        assertEquals("USER_POSTS", rsmd.getColumnLabel(3));
 
         assertTrue(rs.next());
         assertEquals("Zachary", rs.getString(1));
