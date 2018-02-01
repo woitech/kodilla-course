0df629c9871cfed7852eba4bbf3f99eba27921f0
woitech <wojlow@gmail.com>
2018-02-01 16:30:12 +0100

    Poprawki projektu

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,4 +1,4 @@
-package com.kodilla.hibernate.task.dao.tasklist;
+package com.kodilla.hibernate.tasklist.dao;
 
 import com.kodilla.hibernate.tasklist.TaskList;
 import com.kodilla.hibernate.tasklist.dao.TaskListDao;
