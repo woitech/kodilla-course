8d5dbb783db7a00a054a2d949bb5523c6996e139
woitech <wojlow@gmail.com>
2018-03-21 21:57:53 +0100

    20.2 Decorator pattern

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/taxiportal/AbstractTaxiOrderDecorator.java
@@ -0,0 +1,21 @@
+package com.kodilla.patterns2.decorator.taxiportal;
+
+import java.math.BigDecimal;
+
+public class AbstractTaxiOrderDecorator implements TaxiOrder {
+    private TaxiOrder taxiOrder;
+
+    public AbstractTaxiOrderDecorator(TaxiOrder taxiOrder) {
+        this.taxiOrder = taxiOrder;
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return taxiOrder.getCost();
+    }
+
+    @Override
+    public String getDescription() {
+        return taxiOrder.getDescription();
+    }
+}
