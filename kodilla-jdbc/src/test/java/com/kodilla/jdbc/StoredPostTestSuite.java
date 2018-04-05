b5ec68ef08643022f60ffa47a2941d20b6ea2ddc
woitech <wojlow@gmail.com>
2018-04-05 15:36:24 +0200

    Zadanie: Wywołanie procedury UpdateBestsellers z poziomu Javy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
@@ -30,4 +30,26 @@ public class StoredPostTestSuite {
         }
         assertEquals(0, howMany);
     }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        // Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=NULL";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        // When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+
+        // Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER IS NULL";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+    }
 }
